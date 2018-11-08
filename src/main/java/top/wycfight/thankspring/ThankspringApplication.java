41ae4a2962225f282aa2d0eb9c603d2468d5bbbe
wangyanchun <wyc@163.com>
2018-11-08 17:08:24 +0800

    整合Druid未完成

--- src/main/java/top/wycfight/thankspring/ThankspringApplication.java
+++ src/main/java/top/wycfight/thankspring/ThankspringApplication.java
@@ -1,9 +1,13 @@
 package top.wycfight.thankspring;
 
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = DruidDataSourceAutoConfigure.class)
 public class ThankspringApplication {
 
     public static void main(String[] args) {
