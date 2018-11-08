787d078f091060879d9bb24c8b5a62c311cf8744
wangyanchun <wyc@163.com>
2018-11-08 14:23:10 +0800

    第一次提交-创建项目

---
+++ src/main/java/top/wycfight/thankspring/ThankspringApplication.java
@@ -0,0 +1,12 @@
+package top.wycfight.thankspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThankspringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThankspringApplication.class, args);
+    }
+}
